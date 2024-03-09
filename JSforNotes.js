8396788e7d1255d8745a74945ac4ce6da3077bfc
Matthew J <jednacz25@gmail.com>
2024-03-08 19:13:41 -0500

    The list has begun to take shape.

--- JSforNotes.js
+++ JSforNotes.js
@@ -1,9 +1,33 @@
-function textList()
-{
+/*let textList  = document.getElementById('textButton');
+let listContainer = document.getElementById('listContainer');
+let inputField = document.getElementById( 'inputfield')
 
-}
-
-function checkList()
+textList.addEventListener('click', function()
 {
+    var orderedList = document.createElement('ol')
+    orderedList.classList.add('orderedList-styling')
+    orderedList.innerText = inputField.value;
+    toDoContainer.appendChild(orderedList);
+})
+*/
+let inputBx = document.querySelector('#inputBx');
+let list = document.querySelector('#list');
+
+inputBx.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        addItem(this.value);
+        this.value = "";
+    }
+});
+
+function addItem(text) {
+    let listItem = document.createElement("li");
+    listItem.textContent = text; // Use textContent for plain text
+    listItem.innerHTML = `${inputBx}<i></i>`
+    listItem.addEventListener("click", function()
+    {
+        this.classList.toggle('done');
+    })
     
+    list.appendChild(listItem);
 }
